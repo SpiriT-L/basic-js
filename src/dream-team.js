bbae591ed3b4266c4503d752d47f2152bd043a01
SpiriT-L <m.leon-id@yandex.ru>
2021-03-17 22:58:57 +0300

    feat: solved the dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let anagram = [];
+  members.forEach((i) => {
+    if (typeof i === 'string') {
+      anagram.push(i.trim()[0].toUpperCase());
+    }
+  });
+  return anagram.sort().join('');
 };
