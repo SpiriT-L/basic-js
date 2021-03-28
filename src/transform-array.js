a48e9f439396a89bf6419a0281051ebdf583a8fe
SpiriT-L <m.leon-id@yandex.ru>
2021-03-28 15:19:59 +0300

    feat: solving the transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error();
+  }
+  let answer = [];
+  for (let i = 0; i < arr.length; ++i) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          answer.pop();
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) {
+          answer.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          answer.push(arr[i - 1]);
+        }
+        break;
+      default:
+        answer.push(arr[i]);
+    }
+  }
+  return answer;
 };
