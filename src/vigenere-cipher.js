90b2e05b04297d96874c4fa516e2b943e1446141
SpiriT-L <m.leon-id@yandex.ru>
2021-03-28 19:27:01 +0300

    feat: solving the vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  table = [[]];
+  reverse = false;
+  constructor(encrypt) {
+    for (let j = 0; j < 26; j++) {
+      for (
+        let i = String.fromCharCode('A'.charCodeAt(0) + j);
+        this.table[j].length < 26;
+        i = String.fromCharCode(i.charCodeAt(0) + 1)
+      ) {
+        this.table[j].push(i);
+        if (i == 'Z') i = '@';
+      }
+      this.table.push([]);
+    }
+    if (encrypt == false) this.reverse = true;
+  }
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let n = 0;
+    let code = '';
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        let col = message[i].charCodeAt(0) - 65;
+        let row = key.charCodeAt(n % key.length) - 65;
+        code += this.table[row][col];
+        n++;
+      } else code += message[i];
+    }
+    return this.reverse ? code.split('').reverse().join('') : code;
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage == undefined || key == undefined) throw new Error();
+    let result = '';
+    let n = 0;
+    key = key.toUpperCase();
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i].match(/[A-Z]/)) {
+        let row = key.charCodeAt(n % key.length) - 65;
+        let col = this.table[row].indexOf(encryptedMessage[i]);
+        result += this.table[0][col];
+        n++;
+      } else result += encryptedMessage[i];
+    }
+    return this.reverse ? result.split('').reverse().join('') : result;
   }
 }
 
